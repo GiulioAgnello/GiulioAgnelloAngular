a0734096f573bbdae8221bb41b5feb4437dd87cf
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-18 12:24:31 +0100

    routeAdded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Project } from './pages/project/project';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'about',
     component: About,
+  },
+  {
+    path: 'project',
+    component: Project,
   },
   {
     path: 'contact',

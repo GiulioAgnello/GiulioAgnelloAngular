299b9e6d003497f1f14ce9a1921ac2cbc0867d7b
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-18 12:07:51 +0100

    newPageProject

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { Features } from '../../components/features/features';
+import { TechCarouselComponent } from '../../components/tech-carousel/tech-carousel';
 @Component({
   selector: 'app-home',
-  imports: [Hero, Features],
+  imports: [Hero, Features, TechCarouselComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

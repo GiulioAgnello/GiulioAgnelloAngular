cc9173a2735920b5f64b7f77bee8a9659619638e
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-18 12:37:22 +0100

    projectCardAdded

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
